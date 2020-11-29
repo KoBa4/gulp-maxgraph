60ee2d3902fba0f13e7fbf962e14d5cbb5fb8530
KoBa4 <cova44ever@gmail.com>
2020-11-29 07:19:30 +0600

    Fix JS - remove uglify, sourcemap; add terser, babelrc and plugins

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,8 +1,6 @@
 const { src, dest } = require('gulp');
 const webpackStream = require('webpack-stream');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify-es').default;
-const notify = require('gulp-notify');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 const browserSync = require('browser-sync').create();
 
 const scripts = () => {
@@ -11,18 +9,17 @@ const scripts = () => {
       {
         mode: 'development',
         output: {
-          filename: 'main.js',
+          filename: '[name].min.js',
+        },
+        devtool: 'source-map',
+        optimization: {
+          minimize: true,
         },
         module: {
           rules: [{
             test: /\.m?js$/,
             exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
+            use: ['babel-loader'],
           }]
         },
       }
@@ -31,41 +28,41 @@ const scripts = () => {
       console.error('WEBPACK ERROR', err);
       this.emit('end'); // Don't stop the rest of the task
     })
-
-    .pipe(sourcemaps.init())
-    .pipe(uglify().on("error", notify.onError()))
-    .pipe(sourcemaps.write('.'))
     .pipe(dest('./app/js'))
     .pipe(browserSync.stream());
 }
 
 const scriptsBuild = () => {
   return src('./src/js/main.js')
-    .pipe(webpackStream(
-
-      {
-        mode: 'development',
-        output: {
-          filename: 'main.js',
-        },
-        module: {
-          rules: [{
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          }]
-        },
-      }))
+    .pipe(webpackStream({
+      mode: 'production',
+      output: {
+        filename: '[name].min.js',
+      },
+      optimization: {
+        minimize: true,
+        minimizer: [
+          new TerserWebpackPlugin({
+            parallel: true,
+            terserOptions: {
+              format: { comments: false },
+            },
+            extractComments: false,
+          }),
+        ],
+      },
+      module: {
+        rules: [{
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: ['babel-loader'],
+        }]
+      },
+    }))
     .on('error', function (err) {
       console.error('WEBPACK ERROR', err);
       this.emit('end'); // Don't stop the rest of the task
     })
-    .pipe(uglify().on("error", notify.onError()))
     .pipe(dest('./app/js'))
 }
 
