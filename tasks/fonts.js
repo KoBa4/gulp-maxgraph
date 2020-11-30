8e2325546b0212d6cc2015c736928838ace76e1d
KoBa4 <cova44ever@gmail.com>
2020-11-30 13:42:25 +0600

    Fix fonts task

--- tasks/fonts.js
+++ tasks/fonts.js
@@ -6,77 +6,75 @@ let srcFonts = './src/scss/_fonts.scss';
 let appFonts = './app/fonts/';
 const cb = () => {};
 
-const fonts = () => {
-  return src('./src/fonts/**.ttf')
-    .pipe(ttf2woff2())
-    .pipe(dest('./app/fonts/'));
-}
+const fonts = () =>
+  src('./src/fonts/**.ttf').pipe(ttf2woff2()).pipe(dest('./app/fonts/'));
 
-const checkWeight = (fontname) => {
-  let weight = 400;
+const checkWeight = fontName => {
+  let weight;
   switch (true) {
-    case /Thin/.test(fontname):
+    case fontName.endsWith('Thin'):
       weight = 100;
       break;
-    case /ExtraLight/.test(fontname):
+    case /(ExtraLight|UltraLight)$/g.test(fontName):
       weight = 200;
       break;
-    case /Light/.test(fontname):
+    case fontName.endsWith('Light'):
       weight = 300;
       break;
-    case /Regular/.test(fontname):
+    case /(Normal|Regular)$/g.test(fontName):
       weight = 400;
       break;
-    case /Medium/.test(fontname):
+    case fontName.endsWith('Medium'):
       weight = 500;
       break;
-    case /SemiBold/.test(fontname):
+    case /(Semi|SemiBold|DemiBold)$/g.test(fontName):
       weight = 600;
       break;
-    case /Semi/.test(fontname):
-      weight = 600;
-      break;
-    case /Bold/.test(fontname):
+    case fontName.endsWith('Bold'):
       weight = 700;
       break;
-    case /ExtraBold/.test(fontname):
+    case /(ExtraBold|UltraBold)$/g.test(fontName):
       weight = 800;
       break;
-    case /Heavy/.test(fontname):
-      weight = 700;
-      break;
-    case /Black/.test(fontname):
+    case /(Black|Heavy)$/g.test(fontName):
       weight = 900;
       break;
     default:
       weight = 400;
   }
   return weight;
-}
+};
 
-const fontsStyle = (done) => {
-  let file_content = fs.readFileSync(srcFonts);
+const fontsStyle = done => {
+  // eslint-disable-next-line no-unused-vars
+  let fileContent = fs.readFileSync(srcFonts);
 
-  fs.writeFile(srcFonts, '', cb);
+  fs.writeFile(srcFonts, '', undefined, cb);
   fs.readdir(appFonts, function (err, items) {
     if (items) {
-      let c_fontname;
-      for (var i = 0; i < items.length; i++) {
-        let fontname = items[i].split('.');
+      let cFontName = '';
+      // eslint-disable-next-line no-restricted-syntax
+      for (const item of items) {
+        let fontname = item.split('.');
         fontname = fontname[0];
         let font = fontname.split('-')[0];
         let weight = checkWeight(fontname);
 
-        if (c_fontname != fontname) {
-          fs.appendFile(srcFonts, '@include font-face("' + font + '", "' + fontname + '", ' + weight +');\r\n', cb);
+        if (cFontName !== fontname) {
+          fs.appendFile(
+            srcFonts,
+            `@include font-face("${font}", "${fontname}", ${weight});\r\n`,
+            undefined,
+            cb
+          );
         }
-        c_fontname = fontname;
+        cFontName = fontname;
       }
     }
-  })
+  });
 
   done();
-}
+};
 
 module.exports = {
   fonts,
